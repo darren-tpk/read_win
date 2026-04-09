740f7dd78c72d9bec8156994fda8f65503878f02
Darren Tan <ptan@alaska.edu>
2026-04-09 15:08:41 +0900

    Add read_state flag to fall back to sequential reading if unit header byte size does not match channel block size

--- src/read_win/onesecunit.py
+++ src/read_win/onesecunit.py
@@ -1,5 +1,6 @@
 import struct
 import numpy as np
+import warnings
 from obspy import UTCDateTime
 from .utils import _to_signed_array, _uint2int
 
@@ -36,9 +37,29 @@ class OneSecUnit(object):
         # Initialize placeholder for data
         self.data = {}
 
-        # Keep reading channel blocks until the end of the unit
+        # Reading channel blocks until the end of the unit
         while self.cursor_position < self.size_bytes:
-            self._read_channel_block(unit_bytes[self.cursor_position:self.size_bytes])
+
+            # Grab channel bytes
+            channel_bytes = unit_bytes[self.cursor_position:self.size_bytes]
+
+            # Conduct a check to see if the length of unit bytes matches header info
+            self.read_state = "healthy"  # healthy / truncated / fatal
+
+            # Insufficient bytes to derive sensor ID and sampling rate
+            if len(channel_bytes) <= 4:
+                warnings.formatwarning = lambda msg, cat, fname, lineno, line=None: f"{cat.__name__}: {msg}\n"
+                warnings.warn("Insufficient bytes to derive sensor ID and sampling rate. Breaking.", RuntimeWarning)
+                self.read_state = "fatal"
+                break
+
+            # Sensor ID and sampling rate is known, but data length is mismatched
+            # Use read state "truncated" to return np.nan's
+            if (len(channel_bytes)+10) != self.size_bytes:
+                self.read_state = "truncated"
+
+            # Read channel block
+            self._read_channel_block(channel_bytes)
 
     def _read_header(self, unit_bytes):
         """
@@ -80,57 +101,122 @@ class OneSecUnit(object):
         if sensor_id not in self.data:
             self.data[sensor_id] = {"blocks": []}
 
-        # First data sample, always 4 bytes, in absolute units
-        first_sample_value = _uint2int(struct.unpack(">I", channel_bytes[4:8])[0], 4)
-
         # Format strings controls how bytes are unpacked. Correct letter will be selected based on datasize
         format_string = 'BBHBI'
 
-        # Read all remaining bytes at once, but for 0.5 and 3 byte sample size, things are slightly trickier.
-        if datasize_code == 0:  # for 0.5 byte size, data samples are 4-bit but we read 8-bit, so each byte read actually contains two data samples
-            N_samples_to_read = (sampling_rate - 1) // 2
-            N_bytes_to_read = (sampling_rate - 1) // 2
+        # If read state is healthy, proceed
+        if self.read_state == "healthy":
 
-            # If sampling rate is even, e.g. 100 Hz, there are an odd numbers of data samples to read (e.g. 99). But the data size is 0.5 bytes, so the overall size will not be an integer (e.g. 49.5 bytes)
-            # In this case, there will be an additional empty meaningless "padding" 0.5 bytes (i.e. 4 bits) at the end of the
-            if (sampling_rate - 1) % 2:
-                N_samples_to_read += 1
-                N_bytes_to_read += 1
+            # First data sample, always 4 bytes, in absolute units
+            first_sample_value = _uint2int(struct.unpack(">I", channel_bytes[4:8])[0], 4)
 
-            diff_udata_8bit = np.array(struct.unpack('>' + format_string[datasize_code] * N_samples_to_read,
-                                                     channel_bytes[8:8 + N_bytes_to_read]))
+            # Read all remaining bytes at once, but for 0.5 and 3 byte sample size, things are slightly trickier.
+            if datasize_code == 0:  # for 0.5 byte size, data samples are 4-bit but we read 8-bit, so each byte read actually contains two data samples
+                N_samples_to_read = (sampling_rate - 1) // 2
+                N_bytes_to_read = (sampling_rate - 1) // 2
 
-            first_4bit = diff_udata_8bit >> 4
-            second_4bit = diff_udata_8bit & 0xf
-            diff_udata = np.stack((first_4bit, second_4bit), axis=1).flatten()
+                # If sampling rate is even, e.g. 100 Hz, there are an odd numbers of data samples to read (e.g. 99). But the data size is 0.5 bytes, so the overall size will not be an integer (e.g. 49.5 bytes)
+                # In this case, there will be an additional empty meaningless "padding" 0.5 bytes (i.e. 4 bits) at the end of the
+                if (sampling_rate - 1) % 2:
+                    N_samples_to_read += 1
+                    N_bytes_to_read += 1
 
-            # For even sampling rates, the last 4 bits are zeros for padding only and not actual data
-            if (sampling_rate - 1) % 2:
-                diff_udata = diff_udata[:-1]
+                diff_udata_8bit = np.array(struct.unpack('>' + format_string[datasize_code] * N_samples_to_read,
+                                                         channel_bytes[8:8 + N_bytes_to_read]))
 
-        elif datasize_code == 3:  # for 3 byte size, data samples are 24-bit which cannot be read. Instead, we read 8-bit, and combine 3 bytes to get a data sample.
-            N_samples_to_read = (sampling_rate - 1) * 3
-            N_bytes_to_read = N_samples_to_read
-            diff_udata_8bit = np.array(struct.unpack('>' + format_string[datasize_code] * N_samples_to_read,
-                                                     channel_bytes[8:8 + N_bytes_to_read]))
+                first_4bit = diff_udata_8bit >> 4
+                second_4bit = diff_udata_8bit & 0xf
+                diff_udata = np.stack((first_4bit, second_4bit), axis=1).flatten()
 
-            # Recombine each set of three 8-bit value into single 24-bit values
-            first_8bit, second_8bit, third_8bit = np.reshape(diff_udata_8bit, (diff_udata_8bit.shape[0] // 3, 3)).T
-            diff_udata = (first_8bit << 16) + (second_8bit << 8) + third_8bit
+                # For even sampling rates, the last 4 bits are zeros for padding only and not actual data
+                if (sampling_rate - 1) % 2:
+                    diff_udata = diff_udata[:-1]
 
-        else:
-            N_samples_to_read = sampling_rate - 1  # number of data samples remaining to be read
-            N_bytes_to_read = N_samples_to_read * datasize  # number of actual bytes to be read
-            diff_udata = np.array(struct.unpack('>' + format_string[datasize_code] * N_samples_to_read, channel_bytes[8:8 + N_bytes_to_read]))  # reading all remaining data at once. Note that these are differential, unsigned integers
+            elif datasize_code == 3:  # for 3 byte size, data samples are 24-bit which cannot be read. Instead, we read 8-bit, and combine 3 bytes to get a data sample.
+                N_samples_to_read = (sampling_rate - 1) * 3
+                N_bytes_to_read = N_samples_to_read
+                diff_udata_8bit = np.array(struct.unpack('>' + format_string[datasize_code] * N_samples_to_read,
+                                                         channel_bytes[8:8 + N_bytes_to_read]))
 
-        # Convert from unsigned to signed integers, still differential (i.e. difference to previous value rather than absolute value)
-        diff_data = _to_signed_array(diff_udata, datasize)
+                # Recombine each set of three 8-bit value into single 24-bit values
+                first_8bit, second_8bit, third_8bit = np.reshape(diff_udata_8bit, (diff_udata_8bit.shape[0] // 3, 3)).T
+                diff_udata = (first_8bit << 16) + (second_8bit << 8) + third_8bit
 
-        # Convert to absolute units
-        data_segment = np.cumsum(np.insert(diff_data, 0, first_sample_value)).astype(float)
+            else:
+                N_samples_to_read = sampling_rate - 1  # number of data samples remaining to be read
+                N_bytes_to_read = N_samples_to_read * datasize  # number of actual bytes to be read
+                diff_udata = np.array(struct.unpack('>' + format_string[datasize_code] * N_samples_to_read, channel_bytes[8:8 + N_bytes_to_read]))  # reading all remaining data at once. Note that these are differential, unsigned integers
+
+            # Convert from unsigned to signed integers, still differential (i.e. difference to previous value rather than absolute value)
+            diff_data = _to_signed_array(diff_udata, datasize)
+
+            # Convert to absolute units
+            data_segment = np.cumsum(np.insert(diff_data, 0, first_sample_value)).astype(float)
+
+        # Otherwise, read sequentially where possible, then pad with np.nan's
+        elif self.read_state == "truncated":
+            warnings.formatwarning = lambda msg, cat, fname, lineno, line=None: f"{cat.__name__}: {msg}\n"
+            warnings.warn("Mismatch in number of bytes between unit header and channel block.\n"
+                          "Starttime %s; Sensor %s; %d/%d bytes. Falling back to sequential reading; padding with np.nan if needed." %
+                          (self.starttime.strftime("%Y-%m-%d %H:%M:%S"), sensor_id, (len(channel_bytes) + 10),
+                           self.size_bytes), RuntimeWarning)
+
+            # If even the first absolute sample is incomplete, return all nan's
+            if len(channel_bytes) < 8:
+                data_segment = np.full((sampling_rate,), np.nan)
+
+            else:
+                # First data sample, always 4 bytes, in absolute units
+                first_sample_value = _uint2int(struct.unpack(">I", channel_bytes[4:8])[0], 4)
+
+                # Read remaining data carefully, one sample at a time where possible
+                diff_udata_list = []
+                in_block_cursor_position = 8
+
+                if datasize_code == 0:  # 0.5-byte packed data
+                    while len(diff_udata_list) < (sampling_rate - 1):
+                        if in_block_cursor_position >= len(channel_bytes):
+                            break
+                        current_byte = channel_bytes[in_block_cursor_position]
+                        first_4bit = current_byte >> 4
+                        second_4bit = current_byte & 0xf
+                        diff_udata_list.append(first_4bit)
+                        if len(diff_udata_list) < (sampling_rate - 1):
+                            diff_udata_list.append(second_4bit)
+                        in_block_cursor_position += 1
+
+                elif datasize_code == 3:  # 3-byte packed data
+                    while len(diff_udata_list) < (sampling_rate - 1):
+                        if (in_block_cursor_position + 3) > len(channel_bytes):
+                            break
+                        first_8bit, second_8bit, third_8bit = struct.unpack(">BBB", channel_bytes[in_block_cursor_position:in_block_cursor_position + 3])
+                        current_value = (first_8bit << 16) + (second_8bit << 8) + third_8bit
+                        diff_udata_list.append(current_value)
+                        in_block_cursor_position += 3
+
+                else:
+                    while len(diff_udata_list) < (sampling_rate - 1):
+                        if (in_block_cursor_position + datasize) > len(channel_bytes):
+                            break
+                        current_value = struct.unpack(">" + format_string[datasize_code], channel_bytes[in_block_cursor_position:in_block_cursor_position + datasize])[0]
+                        diff_udata_list.append(current_value)
+                        in_block_cursor_position += datasize
+
+                diff_udata = np.array(diff_udata_list)
+
+                # Convert from unsigned to signed integers, still differential (i.e. difference to previous value rather than absolute value)
+                diff_data = _to_signed_array(diff_udata, datasize)
+
+                # Convert recovered portion to absolute units
+                data_segment = np.cumsum(np.insert(diff_data, 0, first_sample_value)).astype(float)
+
+                # Pad remaining samples with np.nan's
+                if len(data_segment) < sampling_rate:
+                    data_segment = np.concatenate((data_segment, np.full((sampling_rate - len(data_segment),), np.nan)))
 
         # Append data segment
-        # Note that the data is still in integers, and not in physical units yet (will be converted all at once, after all 1s units are loaded)
+        # Note that the data is still in integers, and not in physical units yet
+        # Data will be converted to physical units all at once, after all 1s units are loaded
         self.data[sensor_id]["blocks"].append({"starttime": self.starttime,"sampling_rate": sampling_rate, "data": data_segment})
 
         # Update cursor position to end of this channel block
