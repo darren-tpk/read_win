9025344a851a6adc090ba9a1b08d82d634f0a78e
Darren Tan <ptan@alaska.edu>
2026-04-10 11:35:57 +0900

    Update example script to read a gappy example

--- scripts/read_win_example.py
+++ scripts/read_win_example.py
@@ -1,6 +1,8 @@
+# Import dependencies
 from obspy import UTCDateTime
 from read_win import read_win
 
+# Read WIN data from directory
 starttime = UTCDateTime(2026, 3, 5, 5)  # UTC
 endtime = UTCDateTime(2026, 3, 5, 8)  # UTC
 file_directory = "./read_win/sample_data"  # for glob
@@ -8,7 +10,7 @@ file_pattern = "%y%m%d%H"  # UTCDateTime strftime input
 file_interval = "hour"  # "minute", "hour", or "day"
 fill_value = None  # for stream.merge
 channel_table_path = "./read_win/sample_data/channels.tbl"
-verbose = False
+verbose = True
 
 stream = read_win(starttime,
                   endtime,
@@ -18,3 +20,16 @@ stream = read_win(starttime,
                   fill_value,
                   channel_table_path,
                   verbose)
+
+# Read bad WIN data to test warning messages
+starttime = UTCDateTime(2026, 4, 2, 2)  # UTC
+endtime = UTCDateTime(2026, 4, 2, 3)  # UTC
+
+stream_bad = read_win(starttime,
+                      endtime,
+                      file_directory,
+                      file_pattern,
+                      file_interval,
+                      fill_value,
+                      channel_table_path,
+                      verbose)
